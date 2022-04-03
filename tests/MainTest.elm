bff5ea8f0ef7076c7db0f9e8453818f283058ed1
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-04-03 16:05:23 +0900

    fix tests

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -1,11 +1,12 @@
-module MainTest
+module MainTest exposing (..)
 
 import Test exposing (..)
 import Expect exposing (Expectation)
 
 import Main exposing (..)
 
-displayCounterTest : Test =
+displayCounterTest : Test
+displayCounterTest =
   describe "displayCounter"
     [ test "zero" <| \_ ->
         Expect.equal "0" (Main.displayCounter 0)
